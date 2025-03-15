123455bf5a1674bf710014a42669ad3079d61ce3
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-03-15 02:48:59 -0400

    Switch out baseplate for temp scene composed of blocks

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-pub trait NumExt {
+pub mod blocks;
+pub mod player;
+
+pub trait CmpExt {
     fn is_between(&self, lower: Self, upper: Self) -> bool;
 }
 
-impl<T: PartialOrd> NumExt for T {
+impl<T: PartialOrd> CmpExt for T {
     fn is_between(&self, lower: Self, upper: Self) -> bool {
         *self >= lower && *self <= upper
     }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-mod player;
-
 use avian3d::prelude::*;
 use bevy::prelude::*;
 use bevy::window::CursorGrabMode;
-use player::PlayerPlugin;
+use blocks3d::blocks::{BlockTextures, BlockType, set_block, setup_block_materials};
+use blocks3d::player::PlayerPlugin;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default(), PlayerPlugin))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup_block_materials, setup).chain())
         .add_systems(Update, grab_mouse)
         .run();
 }
@@ -16,16 +15,69 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut mats: ResMut<Assets<StandardMaterial>>,
+    btex_reg: Res<BlockTextures>,
+    // mut mats: ResMut<Assets<StandardMaterial>>,
 ) {
     // create baseplate
-    commands.spawn((
-        RigidBody::Static,
-        Collider::cuboid(20.0, 1.0, 20.0),
-        Mesh3d(meshes.add(Cuboid::new(20.0, 1.0, 20.0))),
-        MeshMaterial3d(mats.add(Color::srgb(0.5, 0.55, 0.55))),
-        Transform::from_xyz(0.0, 0.0, 0.0),
-    ));
+    // commands.spawn((
+    //     RigidBody::Static,
+    //     Collider::cuboid(20.0, 1.0, 20.0),
+    //     Mesh3d(meshes.add(Cuboid::new(20.0, 1.0, 20.0))),
+    //     MeshMaterial3d(mats.add(Color::srgb(0.5, 0.55, 0.55))),
+    //     Transform::from_xyz(0.0, 0.0, 0.0),
+    // ));
+
+    for x in -5..=5 {
+        for z in -5..=5 {
+            set_block(
+                BlockType::Grass,
+                (x as f32, 0.0, z as f32),
+                &mut meshes,
+                &btex_reg,
+                &mut commands,
+            );
+        }
+    }
+
+    // Create a tree with a trunk and leaves
+    // Trunk
+    for y in 1..5 {
+        set_block(
+            BlockType::Log,
+            (3.0, y as f32, 3.0),
+            &mut meshes,
+            &btex_reg,
+            &mut commands,
+        );
+    }
+
+    // Leaves
+    for x in 2..=4 {
+        for y in 5..7 {
+            for z in 2..=4 {
+                set_block(
+                    BlockType::Leaves,
+                    (x as f32, y as f32, z as f32),
+                    &mut meshes,
+                    &btex_reg,
+                    &mut commands,
+                );
+            }
+        }
+    }
+
+    // Add a wider leaf layer at bottom of canopy
+    for x in 1..=5 {
+        for z in 1..=5 {
+            set_block(
+                BlockType::Leaves,
+                (x as f32, 4.0, z as f32),
+                &mut meshes,
+                &btex_reg,
+                &mut commands,
+            );
+        }
+    }
 
     // light
     commands.spawn((
